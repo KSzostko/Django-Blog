ff20d5895ff8b7a688d2b55e519f1156c2b04278
Kuba Szóstko <kubaszostko@gmail.com>
2020-05-03 16:05:26 +0200

    added html structure to index

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
 
 
 def index(request):
-    context = {'context': 'hello'}
-    return render(request, 'index.html', context)
+    context = {'blog_list': Blog.objects.all()}
+    return render(request, 'index.html', context=context)
