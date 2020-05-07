b7ff56b19ecc7af0be858fb6d31b32973c8b8ada
Kuba Szóstko <kubaszostko@gmail.com>
2020-05-07 17:45:16 +0200

    added field creator_photo to Blog model adn successfully displayed it on blog_detail

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class Blog(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(default='')
+    creator_photo = models.ImageField(default='user.png')
 
     def __str__(self):
         return f'{self.title}'
